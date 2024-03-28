4cb9b1533098b43f18ee43aeabffa42c4c8f707a
dulee19 <adusan53@gmail.com>
2024-03-28 20:10:04 +0100

    types fix

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -4,13 +4,14 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useWindowSize } from '@/hooks/useWindowSize';
 import { portfolios } from '@/constants'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Swiper as SwiperType } from 'swiper'
 import 'swiper/css'
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import PortfolioItem from './PortfolioItem';
+import { Swiper as SwiperRef } from 'swiper'
+
 
 const PortfolioSection = ({ data, title }: { data: typeof portfolios, title: string }) => {
-    const swiperRef = useRef<SwiperType>();
+    const swiperRef = useRef<SwiperRef>();
     const [slidePerView, setSlidePerView] = useState(3);
 
     const { width } = useWindowSize();
@@ -50,7 +51,7 @@ const PortfolioSection = ({ data, title }: { data: typeof portfolios, title: str
                     spaceBetween={20}
                     slidesPerView={slidePerView}
                     loop={true}
-                    onSwiper={(swiper: string) => (swiperRef.current = swiper)}
+                    onSwiper={(swiper: SwiperRef) => (swiperRef.current = swiper)}
                 >
                     {data.map((portfolio, index) => (
                         <SwiperSlide key={index}>
